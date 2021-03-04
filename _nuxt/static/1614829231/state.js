fe40fda12645eba0151356f1cbf7cea3c68e3b0d
Lucas Santiago <lucasfsanti@gmail.com>
2021-03-04 00:43:10 -0300

    2b0ab3c

---
+++ _nuxt/static/1614829231/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614829231",layout:"default",error:a,state:{color:"#BCF3EA",fontColor:"#1C4752"},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
---
+++ _nuxt/static/1614829231/work/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614829231",layout:"default",error:a,state:{color:"#BCF3EA",fontColor:"#1C4752"},serverRendered:true,routePath:"\u002Fwork",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
